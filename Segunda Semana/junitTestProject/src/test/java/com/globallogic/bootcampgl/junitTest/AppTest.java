1764bfb159d920b71aef81934d21c909fa420fce
Leandro Javier Baier <leanbaieruni@gmail.com>
2020-07-30 13:50:23 -0300

    Ejercicios 1.* faltantes

--- Segunda Semana/junitTestProject/src/test/java/com/globallogic/bootcampgl/junitTest/AppTest.java
+++ Segunda Semana/junitTestProject/src/test/java/com/globallogic/bootcampgl/junitTest/AppTest.java
@@ -51,7 +51,7 @@ public class AppTest {
 	}
 	
 	//EJERCICIO 1.5 -------------------------------------
-
+	
 	@Test
 	@DisplayName("Ejercicio 1.2 F")
 	void test2f()
@@ -81,5 +81,43 @@ public class AppTest {
 		assertNull(numero);
 	}
 	
+	// -----------------------------------
+	
+	@Test
+	@DisplayName("Ejercicio 1.6")
+	void test6()
+	{
+		int numero1 = 5, numero2 = 6;
+		int suma;
+		suma = numero1 + numero2;
+		assertEquals("El resultado esta mal: ",suma, suma(numero1,numero2));
+	}
+	
+	private int suma (int num1, int num2)
+	{
+		return num1 + num2;
+	}
+	
+	//----------------------------------------
+	
+	@Test
+	@DisplayName("Ejercicio 1.7")
+	void test7()
+	{
+		assertTrue(iteracion(5), "Esta variable no es true" );
+		assertFalse(iteracion(2), "Esta variable no es false");
+	}
+	
+	private boolean iteracion(int cantidadIteraciones)
+	{
+		for (int i = 0; i < cantidadIteraciones; i++)
+		{
+			if (i == 3)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 	
 }
