aea0eb1a141ae4c203fa4d7ea17d502e6e8738ae
Leandro Javier Baier <leanbaieruni@gmail.com>
2020-08-03 09:15:47 -0300

    Todos los ejercicios terminados

---
+++ Segunda Semana/mockito-example-calculator/src/main/java/mockito/example/services/exceptions/ZeroDivisionException.java
@@ -0,0 +1,14 @@
+package mockito.example.services.exceptions;
+
+public class ZeroDivisionException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String mensaje = "No puede dividir por 0";
+	
+	public ZeroDivisionException() {
+		// TODO Auto-generated constructor stub
+	}
+}
